1ddd40c01418d75dd918138594b46180146f211f
fedichebbi <35900204+fedichebbi@users.noreply.github.com>
2018-02-06 18:21:09 +0100

    All User Entities

--- src/Entities/EntityUser.java
+++ src/Entities/EntityUser.java
@@ -10,5 +10,96 @@ package Entities;
  * @author Fedi
  */
 public class EntityUser {
+    int id;
+    String pseudo;
+    String password;
+    String nom;
+    String prenom;
+    String email;
+    String adresse;
+    String date_naissance;
+    
+    public EntityUser()
+    {
+        
+    }
+
+    public EntityUser(int id, String pseudo, String password, String nom, String prenom, String email, String adresse, String date_naissance) {
+        this.id = id;
+        this.pseudo = pseudo;
+        this.password = password;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.adresse = adresse;
+        this.date_naissance = date_naissance;
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public String getDate_naissance() {
+        return date_naissance;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public void setDate_naissance(String date_naissance) {
+        this.date_naissance = date_naissance;
+    }
+    
     
 }
